0e04b1c259e32aacab8cf9ddd33d9f3d295499ff
dedantas <deivid.dantas2012@gmail.com>
2026-05-26 18:28:26 +0100

    adicionando e testando a minilibx (Makefile e include), abrindo uma janela vazia

--- includes/cub3D.h
+++ includes/cub3D.h
@@ -2,6 +2,7 @@
 #ifndef CUB3D_H
 # define CUB3D_H
 
+# include <mlx.h>
 # include <errno.h>
 # include <fcntl.h>
 # include <math.h>
--- src/main.c
+++ src/main.c
@@ -1,9 +1,27 @@
 #include "../includes/cub3D.h"
 
-int	main(int ac, char **av)
+/*int	main(int ac, char **av)
 {
 	if (ac==1)
 		write(1,"ok\n", 4);
 	else
 		printf("%s\n", av[1]);
+}*/
+
+int	main(int ac, char **av)
+{
+	(void)ac;
+	(void)av;
+	void	*mlx;
+
+	mlx = mlx_init();
+	if (!mlx)
+        {
+                write(2, "mlx_init() failed\n", 18);
+                return (1);
+        }
+        write(1, "mlx_init() ok\n", 14);
+        mlx_new_window(mlx, 800, 600, "Hello world");
+        write(1, "window created\n", 15);
+        mlx_loop(mlx);
 }
