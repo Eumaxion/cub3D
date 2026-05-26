fed92bd8b96103c866e7fcfcd1b57776f1a5cbaa
dedantas <deivid.dantas2012@gmail.com>
2026-05-26 19:29:48 +0100

    adicionado key_hook ESC e X para fechar tela

--- src/main.c
+++ src/main.c
@@ -8,20 +8,46 @@
 		printf("%s\n", av[1]);
 }*/
 
-int	main(int ac, char **av)
+int	key_hook(int keycode, void *param)
 {
-	(void)ac;
-	(void)av;
-	void	*mlx;
+	(void)param;
+	if (keycode == 65307) // codigo ESC
+	{
+		printf("Game closed by user\n");
+		exit(1);
+	}
+	return (0);
+}
 
-	mlx = mlx_init();
-	if (!mlx)
-        {
-                write(2, "mlx_init() failed\n", 18);
-                return (1);
-        }
-        write(1, "mlx_init() ok\n", 14);
-        mlx_new_window(mlx, 800, 600, "Hello world");
-        write(1, "window created\n", 15);
-        mlx_loop(mlx);
+int     close_hook(void)
+{
+        printf("Game closed by window\n");
+        exit(0);
+}
+
+int     main(int ac, char **av)
+{
+    void    *mlx;
+    void    *win;
+
+    (void)ac;
+    (void)av;
+    mlx = mlx_init();
+    if (!mlx)
+    {
+        write(2, "mlx_init() failed\n", 18);
+        return (1);
+    }
+    write(1, "mlx_init() ok\n", 14);
+    win = mlx_new_window(mlx, 800, 600, "cub3D");
+    if (!win)
+    {
+        write(2, "mlx_new_window() failed\n", 24);
+        return (1);
+    }
+    write(1, "window created\n", 15);
+    mlx_key_hook(win, key_hook, NULL);
+    mlx_hook(win, 17, 0, close_hook, NULL);
+    mlx_loop(mlx);
+    return (0);
 }
